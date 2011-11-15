00683d7b6ef58d394db24d577fdd0bc539cd061b
Roman Birg <romanbirg@gmail.com>
2011-11-15 12:38:43 -0600

    late init commit

---
+++ src/com/android/systemui/statusbar/CallOnGoingView.java
@@ -0,0 +1,17 @@
+
+package com.android.systemui.statusbar;
+
+import android.content.Context;
+import android.view.View;
+
+public class CallOnGoingView extends View {
+
+    StatusBarService mService;
+    
+    CallOnGoingView(Context c) {
+        super(c);
+        // TODO Auto-generated constructor stub
+    }
+
+}
+
---
+++ src/com/android/systemui/statusbar/quickpanel/QuickSettingsView.java
@@ -0,0 +1,19 @@
+package com.android.systemui.statusbar.quickpanel;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.systemui.statusbar.StatusBarService;
+
+
+public class QuickSettingsView extends View{
+    
+
+
+    public QuickSettingsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+}
